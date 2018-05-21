01dce196e242020967a6d58260ef1e6369372510
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-05-21 17:07:49 +0400

    currencey subview of textfield added

--- Window Shopper/Window Shopper/Views/CurrencyTextField.swift
+++ Window Shopper/Window Shopper/Views/CurrencyTextField.swift
@@ -11,6 +11,26 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 25
+        let currLbl = UILabel(frame: CGRect(x: 20, y: frame.size.height/2 - size/2 , width: size, height: size))
+        currLbl.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        currLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)
+        currLbl.textAlignment = .center
+        
+        currLbl.clipsToBounds = true
+        currLbl.layer.cornerRadius = 5
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currLbl.text = formatter.currencySymbol
+        
+        addSubview(currLbl)
+        
+        
+    }
+    
     override func prepareForInterfaceBuilder() {
         self.backgroundColor=#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2472442209)
         layer.cornerRadius = 5
